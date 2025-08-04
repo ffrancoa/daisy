7b814a0f9e242d273f516831eae680848f281d7f
ffrancoa <ffrancoa97@gmail.com>
2025-08-03 22:31:03 -0500

    Change print to echo.

--- src/daisy/writer.py
+++ src/daisy/writer.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 
+from rich_click import echo
+
 OUTPUT_FILENAME = "lib.rs"
 
 def write_to_file(content: str, filename: str = OUTPUT_FILENAME) -> None:
     path = Path(filename)
     path.write_text(content + "\n", encoding="utf-8")
-    print(f"Written to {filename}")
+    echo(f"ok: written to '{filename}'")
